ebb15f8ffd083da193c29bab790576be5a85b9e5
jyshum <jaredshum101@gmail.com>
2026-03-21 19:54:46 -0700

    add homepage and request form

---
+++ app/request/page.tsx
@@ -0,0 +1,13 @@
+export default function RequestPage() {
+  return (
+    <main>
+      <h1>Request Flowers</h1>
+      <iframe
+        src="https://tally.so/embed/RGLO94"
+        width="100%"
+        height="600"
+        title="Flower Request Form"
+      />
+    </main>
+  )
+}
